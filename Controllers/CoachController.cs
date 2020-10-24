b76af44b31bed2e359b628c73359920888c5db50
Trent Lyons <tlyons1@cougars.ccis.edu>
2020-10-23 21:43:59 -0500

    Comented Out

--- Controllers/CoachController.cs
+++ Controllers/CoachController.cs
@@ -143,31 +143,32 @@ namespace CourseProject.Controllers
             db.SaveChanges();
             return RedirectToAction("SessionByCoach");
         }
+        //Commented out - SqlException: The UPDATE statement conflicted with the FOREIGN KEY constraint "FK_Sessions_Coaches_CoachId". The conflict occurred in database "SwimmingSchoolDb", table "dbo.Coaches", column 'CoachId'. The statement has been terminated.
         //public IActionResult EditSession(int id)
         //{
-           // Session session;
-            //session = db.Sessions.Find(id);
-            //return View(session);
+        // Session session;
+        //session = db.Sessions.Find(id);
+        //return View(session);
         //}
         //[HttpPost]
         //public IActionResult EditSession(Session session)
         //{
-            //db.Update(session);
-            //db.SaveChanges();
-            //return RedirectToAction("SessionByCoach");
+        //db.Update(session);
+        //db.SaveChanges();
+        //return RedirectToAction("SessionByCoach");
         //}
         //public IActionResult DeleteSession(int id)
         //{
-           // Session session;
-            //session = db.Sessions.Find(id);
-            //return View(session);
+        // Session session;
+        //session = db.Sessions.Find(id);
+        //return View(session);
         //}
         //[HttpPost]
-       // public IActionResult DeleteSession(Session session)
+        // public IActionResult DeleteSession(Session session)
         //{
-           // db.Remove(session);
-           // db.SaveChanges();
-           // return RedirectToAction("SessionByCoach");
+        // db.Remove(session);
+        // db.SaveChanges();
+        // return RedirectToAction("SessionByCoach");
         //}
     }
 }
